63b21a56df91a10c2673609730d2c3129647290f
Timo Schmidt <timo.hund@yahoo.com>
2017-06-27 12:02:36 +0200

    Set bin directory

--- Build/Test/publish_coverage.sh
+++ Build/Test/publish_coverage.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+if [ $TRAVIS ]; then
+    # Travis does not have composer's bin dir in $PATH
+    export PATH="$PATH:$HOME/.composer/vendor/bin"
+fi
+
 ocular code-coverage:upload --format=php-clover coverage.unit.clover
 ocular code-coverage:upload --format=php-clover coverage.functional.clover
