98d21546744f89710c700185afc1fc8573dd9d2f
Timo Schmidt <timo.hund@yahoo.com>
2017-06-27 14:08:30 +0200

    Added TYPO3_BIN_DIR

--- Build/Test/cibuild.sh
+++ Build/Test/cibuild.sh
@@ -4,18 +4,13 @@ echo "PWD: $(pwd)"
 
 export TYPO3_PATH_WEB="$(pwd)/.Build/Web/"
 export TYPO3_PATH_PACKAGES="$(pwd)/.Build/vendor/"
+export TYPO3_BIN_DIR="$(pwd)/.Build/bin/"
 
-if [ $TRAVIS ]; then
-    # Travis does not have composer's bin dir in $PATH
-    export PATH="$PATH:$HOME/.composer/vendor/bin"
-fi
-
-ls -l .Build/bin/
+export PATH="$PATH:$HOME/.composer/vendor/bin:$TYPO3_BIN_DIR"
 
 echo "Run PHP Lint"
 find . -name \*.php ! -path "./.Build/*" | parallel --gnu php -d display_errors=stderr -l {} > /dev/null \;
 
-# use from vendor dir
 php-cs-fixer --version > /dev/null 2>&1
 if [ $? -eq "0" ]; then
     echo "Check PSR-2 compliance"
@@ -30,12 +25,11 @@ fi
 
 echo "Run unit tests"
 UNIT_BOOTSTRAP=".Build/vendor/nimut/testing-framework/res/Configuration/UnitTestsBootstrap.php"
-.Build/bin/phpunit --colors -c Build/Test/UnitTests.xml --coverage-clover=coverage.unit.clover --bootstrap=$UNIT_BOOTSTRAP
+phpunit --colors -c Build/Test/UnitTests.xml --coverage-clover=coverage.unit.clover --bootstrap=$UNIT_BOOTSTRAP
 if [ $? -ne "0" ]; then
     echo "Error during running the unit tests please check and fix them"
     exit 1
 fi
-
 
 #
 # Map the travis and shell variable names to the expected
@@ -71,4 +65,4 @@ fi
 
 echo "Run functional tests"
 FUNCTIONAL_BOOTSTRAP=".Build/vendor/nimut/testing-framework/res/Configuration/FunctionalTestsBootstrap.php"
-.Build/bin/phpunit --colors -c Build/Test/FunctionalTests.xml --coverage-clover=coverage.functional.clover --bootstrap=$FUNCTIONAL_BOOTSTRAP
+phpunit --colors -c Build/Test/FunctionalTests.xml --coverage-clover=coverage.functional.clover --bootstrap=$FUNCTIONAL_BOOTSTRAP
